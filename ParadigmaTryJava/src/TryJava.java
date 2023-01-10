dbbfe6ec21189ccb6bf211090f6036d40ec679e5
EijiAckerman <122338964+EijiAckermanZ@users.noreply.github.com>
2023-01-10 12:42:17 +0700

    Add files via upload

---
+++ ParadigmaTryJava/src/TryJava.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aji
+ */
+public class TryJava {
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Prasetya Aji Yudhistira");
+        System.out.println("Prodi Teknik Informatika");
+    }
+}
